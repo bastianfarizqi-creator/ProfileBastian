fa51dfaab2e13b2edbc9f38c3d5d3ad01a331a14
rasyid651 <muhammadalrasyid789@gmail.com>
2026-04-27 20:06:35 +0700

    pushhh

---
+++ app/src/main/java/com/example/profilebastian/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.profilebastian
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+// import library
+import android.widget.Button
+import android.content.Intent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val btnNext = findViewById<Button>(R.id.btnLanjut)
+        // membuat fungsi klik pada button
+        btnNext.setOnClickListener {
+            val intent = Intent(this, Profile2::class.java)
+            startActivity(intent)
+        }
+    }
+}
